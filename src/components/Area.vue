df2f7cfa751eae5489b94bd27a580d996ee2108f
Denis <guiraudet@live.fr>
2018-12-06 21:57:51 +0100

    init all components

---
+++ src/components/Area.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="area">
+        <Tile/>
+    </div>
+</template>
+
+<script>
+import Tile from '@/components/Tile'
+
+export default {
+    name: 'Area',
+    components: {
+        Tile
+    },
+
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .area {
+    }
+</style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="tile">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Tile',
+
+        data () {
+            return {
+                img: '',
+                traversable: true,
+                lifecost: 0
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .tile {
+    }
+</style>
