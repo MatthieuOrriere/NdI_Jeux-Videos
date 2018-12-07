50cdbef0360447049ee8f587810426cf3dc2f067
Kamideux <matthieu.orriere@gmail.com>
2018-12-07 03:42:34 +0100

    Add tile

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -22,8 +22,23 @@
         flex: 1;
         background-color: white;
         border: solid 1px black;
-        &.plain-grass {
-            background-image: url("../assets/plain/grass.png");
+        &.plain-grass-tile{
+            background-image: url("../assets/plain/grass-tile.png");
+        }
+        &.plain-grass-tile-second {
+            background-image: url("../assets/plain/grass-tile-second.png");
+        }
+        &.plain-blue-ground {
+            background-image: url("../assets/plain/blue-ground.png");
+        }
+        &.plain-flowers {
+            background-image: url("../assets/plain/flowers.png");
+        }
+        &.block-bricks {
+            background-image: url("../assets/block/bricks.png");
+        }
+        &.block-sea {
+            background-image: url("../assets/block/sea.png");
         }
         background-size: cover;
     }
